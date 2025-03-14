d996ecf62782b200fc86f15bcca12be818b16287
Juan Rodriguez <DeveloperJuanR@gmail.com>
2025-03-14 06:13:10 -0400

    Initial Commit

---
+++ Patient-Tracker/Patient_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Patient_TrackerApp.swift
+//  Patient-Tracker
+//
+//  Created by Juan Rodriguez on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Patient_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
